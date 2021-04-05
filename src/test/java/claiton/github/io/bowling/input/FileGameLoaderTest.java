076ea1a8b669e7ecc53cd2c01265ea231ea7266e
Claiton de Melo Marcilio <claiton.marcilio@betha.com.br>
2021-04-05 18:35:43 +0200

    File load unit tests

--- src/test/java/claiton/github/io/bowling/input/FileGameLoaderTest.java
+++ src/test/java/claiton/github/io/bowling/input/FileGameLoaderTest.java
@@ -1,5 +1,6 @@
 package claiton.github.io.bowling.input;
 
+import claiton.github.io.bowling.exception.ValidationException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -11,5 +12,21 @@ public class FileGameLoaderTest {
 
         Assertions.assertDoesNotThrow(() ->
                 gameLoader.loadGame("src/test/resources/small-game.txt"));
+    }
+
+    @Test
+    void shouldFailWhenInvalidLine() {
+        final GameLoader gameLoader = new FileGameLoader();
+
+        Assertions.assertThrows(ValidationException.class, () ->
+                gameLoader.loadGame("src/test/resources/invalid-line.txt"));
+    }
+
+    @Test
+    void shouldFailWhenInvalidScore() {
+        final GameLoader gameLoader = new FileGameLoader();
+
+        Assertions.assertThrows(ValidationException.class, () ->
+                gameLoader.loadGame("src/test/resources/invalid-score.txt"));
     }
 }
